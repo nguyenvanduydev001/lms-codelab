bc1b2f85255eddc33a19bf956e11aac52506fb5e
nguyenvanduydev001 <nguyenvanduydev001@gmail.com>
2025-02-01 21:11:42 +0700

    Chapter 2

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -1,8 +1,11 @@
 // import React from 'react'
 
+import CallToAction from "../../components/student/CallToAction";
 import Companies from "../../components/student/Companies";
 import CoursesSection from "../../components/student/CoursesSection";
+import Footer from "../../components/student/Footer";
 import Hero from "../../components/student/Hero";
+import TestimonialsSection from "../../components/student/TestimonialsSection";
 
 const Home = () => {
   return (
@@ -10,6 +13,9 @@ const Home = () => {
       <Hero />
       <Companies />
       <CoursesSection />
+      <TestimonialsSection />
+      <CallToAction />
+      <Footer />
     </div>
   );
 };
